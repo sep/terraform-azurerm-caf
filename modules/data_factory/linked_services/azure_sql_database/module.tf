bd0191cbfad8ce2bb022aa04744a8312ed93fe37
Jason Dossett <87334399+jtdossett@users.noreply.github.com>
2023-02-03 16:26:10 -0500

    Update module.tf

--- modules/data_factory/linked_services/azure_sql_database/module.tf
+++ modules/data_factory/linked_services/azure_sql_database/module.tf
@@ -12,7 +12,7 @@ resource "azurerm_data_factory_linked_service_azure_sql_database" "linked_servic
   resource_group_name      = var.resource_group_name
   data_factory_id          = var.data_factory_id
   description              = try(var.settings.description, null)
-  integration_runtime_name = try(var.settings.integration_runtime_name, null)
+  integration_runtime_name = try(var.integration_runtime_name, null)
   annotations              = try(var.settings.annotations, null)
   parameters               = try(var.settings.parameters, null)
   additional_properties    = try(var.settings.additional_properties, null)
